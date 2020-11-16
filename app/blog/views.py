37cd51f02863e13de436bdb19dbb50f7951e3bb0
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-11-16 22:22:12 +0900

    カテゴリー機能のviewを追加

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import BaseModel
+from .models import BaseModel, Category
 
+
+def CategoryFunc(request, category):
+    category = Category.objects.get(name=category)
+    blogCategory = BaseModel.objects.filter(category=category)
+    return render(request, 'top.html', { 'category': category, 'blogCategory': blogCategory, 'test': test })
 
 def BlogFunc(request):
     object_list = BaseModel.objects.all()
