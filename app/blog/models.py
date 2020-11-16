76aad0655e02fcfc0d18db6a851e7b22f86fd639
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-11-16 22:21:38 +0900

    カテゴリーのモデル情報を追加

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
+# カテゴリーモデル
+class Category(models.Model):
+    name = models.CharField('カテゴリー', max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class BaseModel(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category, verbose_name = 'カテゴリー',
+        on_delete = models.PROTECT
+    )
 
     def __str__(self):
         return self.title
-    
