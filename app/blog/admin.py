f84e97cb0b55ae8d511918d9cd0a71e8abab8db1
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-11-22 17:01:30 +0900

    WYSIWYGエディターをadminで使えるように

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import BaseModel, Category
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(BaseModel)
+class BaseModelAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text',)
+
+admin.site.register(BaseModel, BaseModelAdmin)
 admin.site.register(Category)
+
