ebb653b368ce7115b4860466df19180fdeea93b7
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-11-22 17:06:49 +0900

    日本語に説明を変更

--- app/blog/models.py
+++ app/blog/models.py
@@ -16,16 +16,16 @@ class DiaryQuerySet(models.QuerySet):
 
 # 投稿機能のモデル
 class BaseModel(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=25, null=True)
-    text = models.TextField()
+    title = models.CharField('タイトル', max_length=50)
+    description = models.CharField('説明（25文字）', max_length=25, null=True)
+    text = models.TextField('メインコンテンツ')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(
         Category, verbose_name = 'カテゴリー',
         on_delete = models.PROTECT
     )
-    publish_at = models.DateTimeField('作成日', default=timezone.now)
+    publish_at = models.DateTimeField('予約時間', default=timezone.now)
     objects = DiaryQuerySet.as_manager()
 
     def __str__(self):
