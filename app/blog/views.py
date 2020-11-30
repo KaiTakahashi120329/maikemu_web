c77f50d224f24448091df34415d62db4f9b1ffa0
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-11-30 09:48:13 +0900

    views.py編集

--- app/blog/views.py
+++ app/blog/views.py
@@ -6,7 +6,7 @@ import datetime
 def CategoryFunc(request, category):
     category = Category.objects.get(name=category)
     blogCategory = BaseModel.objects.filter(category=category)
-    return render(request, 'top.html', { 'category': category, 'blogCategory': blogCategory})
+    return render(request, 'category.html', { 'category': category, 'blogCategory': blogCategory})
 
 def BlogFunc(request):
     publish_list = BaseModel.objects.published()
