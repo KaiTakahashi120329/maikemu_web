0041b5d4a620886ac82b1d36467d95bd433ad0cd
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-12-04 23:35:42 +0900

    お問い合わせのルーティング

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BlogFunc, CategoryFunc, DetailFunc
+from .views import BlogFunc, CategoryFunc, DetailFunc, ContactView, ContentView, SendEmailView, SuccessFunc
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', BlogFunc, name='list'),
     path('category/<str:category>', CategoryFunc, name='category'),
     path('detail/<int:pk>/', DetailFunc, name='detail'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('contact/content/', ContentView.as_view(), name='content'),
+    path('contact/send_email/', SendEmailView.as_view(), name='send'),
+    path('contact/success/', SuccessFunc, name='success'),
 ]
